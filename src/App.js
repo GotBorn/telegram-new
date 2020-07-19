08c491bdadcf3779cd360aa74efea2cdf5a0ae3b
egorov-aleksandr <56123126+egorov-aleksandr@users.noreply.github.com>
2020-07-19 22:44:55 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Authorization from './Authorization'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = {
+        token: localStorage.getItem('token'),
+    };
 
-export default App;
+    onLogin = (token) =>{
+        localStorage.setItem('token', token)
+        this.setState({token : token})
+    }
+
+    render() {
+        if (!this.state.token)
+            return <Authorization onLogin = {this.onLogin}/>
+        else
+            return <Authorization/>   // здесь будет возвращаться окно с чатом
+    }   
+}   
+export default App
--- src/Authorization.js
+++ src/Authorization.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import './style.css';
+import React, { Component } from 'react'
+import './Auth.css'
 
-function App() {
-  return (
-      <div class = "Background">
-          <div class = "AuthorizationWindow">
-              <p>Логин</p>
-              <input id = "LoginConteiner" type="text" size="40"></input>
-              <p>Пароль</p>
-              <input id = "PasswordConteiner" type="text" size="40"></input>
-              <br></br>
-              <button id = "AuthButton">Войти</button>
-          </div>
-      </div>
-  );
-}
+class Authorization extends Component {
+    state = {
+        login: '',
+        password: '',
+        clicked: false,
+        error: '',
+    };
 
-export default App;
+    handleLogin = (event) => {
+        this.setState({ login: event.target.value });
+    }
+
+    handlePassword = (event) => {
+        this.setState({ password: event.target.value });
+    }
+// здесь будет отправляться логин с паролем в бэкенд
+    /*onClick = async () => {
+        this.setState({ clicked: true, error:  "" });
+        try {
+            let token = await 
+            this.props.onLogin(token);
+        }
+        catch (error) {
+            this.setState({error: error.message});
+        }
+        this.setState({ clicked: false});
+    }*/
+};
+
+render() {
+    return (
+    <div class = "Background">
+    <div class = "AuthorizationWindow">
+    <p>Логин</p>
+    <input id = "LoginConteiner" type="text" size="40" value = {this.state.login} onChange = {this.handleLogin}></input>
+    <p>Пароль</p>
+    <input id = "PasswordConteiner" type="text" size="40" value={this.state.password} onChange={this.handlePassword} ></input>
+    <br></br>
+    <button id = "AuthButton">Войти</button>
+    </div>
+    </div>
+    );
+    }
+
+    export default Authorization
