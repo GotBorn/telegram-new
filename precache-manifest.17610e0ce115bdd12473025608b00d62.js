8631e157139c52f6c4e7f1d3507135e6cf3f8c8a
Alexandr Utkov <maxomega6@rambler.ru>
2020-07-20 23:32:19 +0500

    Updates

---
+++ precache-manifest.17610e0ce115bdd12473025608b00d62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4591b2379627ed90ab9494fa5e688f",
+    "url": "/telegram-new/index.html"
+  },
+  {
+    "revision": "7c2613d40b648d230e98",
+    "url": "/telegram-new/static/css/main.fcc81856.chunk.css"
+  },
+  {
+    "revision": "2fb3b6b8cfe6d8452e3a",
+    "url": "/telegram-new/static/js/2.8bd249a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/telegram-new/static/js/2.8bd249a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2613d40b648d230e98",
+    "url": "/telegram-new/static/js/main.f5d3b51c.chunk.js"
+  },
+  {
+    "revision": "49a540af263b9bdca657",
+    "url": "/telegram-new/static/js/runtime-main.66e7e1e6.js"
+  }
+]);
